76ff29255d4f52dfd151944fed72d8939f4176b3
Cengiz Efe Korkmazer <116306911+CengizReis88@users.noreply.github.com>
2024-02-06 22:37:32 +0300

    sa

---
+++ PHP and MYSQL Integration/mainpage.php
@@ -0,0 +1,11 @@
+
+<head>
+    <title>Welcome to TODO Table</title>
+</head>
+<body>
+    <a href="index.php" target="_blank" onclick="closeCurrentPage()";>Sayfa 1</a>
+    <script>
+        displayTodos($conn,$username);
+    </script>    
+</body>
+</html>
