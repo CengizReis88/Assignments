b36e4fbe0fe9fe86a64446094f4f72cc4569c522
Cengiz Efe Korkmazer <116306911+CengizReis88@users.noreply.github.com>
2024-02-11 21:22:24 +0300

    a

---
+++ Web Development with PHP and JavaScript/index.php
@@ -0,0 +1,14 @@
+
+<html>
+<head>
+    <title>Interactive Page</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="index.js"></script>
+</head>
+<body>
+    <div id=element class="hidden">Biz burdayız</div>
+    <button class="button" id="show-element">Show Element</button>
+    <button class="button" id="hide-element">Hide Element</button>
+</body>
+</html>
