337224de6dd48c4700c9af7f1d8307ee74abb433
Cengiz Efe Korkmazer <116306911+CengizReis88@users.noreply.github.com>
2024-02-12 00:07:52 +0300

    a

---
+++ Web Development with PHP and JavaScript/Web Development with PHP and JavaScript/index.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+
+    $('#show-element').click(function() {
+        $('#element').removeClass('hidden');
+    });
+
+
+    $('#hide-element').click(function(){
+       $('#element').addClass('hidden');
+    });
+        
+});
+
---
+++ Web Development with PHP and JavaScript/Web Development with PHP and JavaScript/index.php
@@ -0,0 +1,14 @@
+
+<html>
+<head>
+    <title>Interactive Page</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="index.js"></script>
+</head>
+<body>
+    <div id=element class="hidden">Biz burdayız</div>
+    <button class="buttonfade1" id="show-element">Show </button>
+    <button class="buttonfade2" id="hide-element">Hide </button>
+</body>
+</html>
